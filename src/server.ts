101f38383e2577ba55a9c093d259bceed1cf19b5
Ricardo Gorki <ricardo.gorki@gmail.com>
2021-02-25 17:12:37 -0300

    surveys and tests

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,6 @@
-import 'reflect-metadata';
-import express from 'express'
-import routes from './routes'
-
-import './database'
-
-const app = express()
-
-app.use(express.json())
-
-app.use(routes)
+import app from './app'
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
 });
 
-export default app;
