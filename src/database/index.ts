cee88e4543a77a711db05fb8e8dc768a5cabb609
Ricardo Gorki <ricardo.gorki@gmail.com>
2021-02-27 02:54:35 -0300

    surveys nodemailer

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,11 +2,11 @@ import { Connection, createConnection, getConnectionOptions } from 'typeorm'
 
 export default async(): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
-  return createConnection(
-    Object.assign(defaultOptions, {
-      database: process.env.NODE_ENV === 'test'
-      ? "./src/database/database.test.sqlite"
-      : defaultOptions.database
-    })
-  )
+  return createConnection()
 }
+
+// Error windows, devido a variavel ambiente.
+//Object.assign(defaultOptions, {
+ // database: process.env.NODE_ENV === 'test'
+ // ? "./src/database/database.test.sqlite"
+ // : defaultOptions.database
--- src/routes.ts
+++ src/routes.ts
@@ -2,14 +2,21 @@ import { Router } from 'express'
 
 import UserController from './controllers/UserController'
 import SurveysController from './controllers/SurveysController'
+import SendMailController from './controllers/SendMailController'
 
 const routes = Router()
 
 const userController = new UserController()
 const surveysController = new SurveysController()
+const sendMailController = new SendMailController()
 
 routes.post('/users', userController.create)
+
+
 routes.post('/surveys', surveysController.create)
 routes.get('/surveys', surveysController.show)
+
+
+routes.post('/sendMail', sendMailController.execute)
 
 export default routes;
