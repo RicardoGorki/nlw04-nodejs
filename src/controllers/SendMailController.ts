e37453259b477905e8138ac2ff3972cb16a1e464
Ricardo Gorki <ricardo.gorki@gmail.com>
2021-02-27 20:48:28 -0300

    hbs fix email

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -5,42 +5,66 @@ import SurveysUsersRepository from "../repositories/SurveysUsersRepository";
 import UsersRepository from "../repositories/UsersRepository";
 import SendMailService from "../services/SendMailService";
 
+import { resolve } from "path";
 
 class SendMailController {
   async execute(request: Request, response: Response) {
-    const { email, survey_id} = request.body;
+    const { email, survey_id } = request.body;
 
-    const usersRepository = getCustomRepository(UsersRepository)
-    const surveysRepository = getCustomRepository(SurveysRepository)
-    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository)
+    const usersRepository = getCustomRepository(UsersRepository);
+    const surveysRepository = getCustomRepository(SurveysRepository);
+    const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
 
-    const userAlreadyExists = await usersRepository.findOne({email})
+    const user = await usersRepository.findOne({ email });
 
-    if(!userAlreadyExists) {
+    if (!user) {
       return response.status(400).json({
-        error:"User does not exists"
-      })
+        error: "User does not exists",
+      });
     }
 
-    const survey = await surveysRepository.findOne({id: survey_id})
+    const survey = await surveysRepository.findOne({ id: survey_id });
 
-    if(!survey) {
+    if (!survey) {
       return response.status(400).json({
-        error:"Survey does not exists"
-      })
+        error: "Survey does not exists",
+      });
+    }
+
+    const variables = {
+      name: user.name,
+      title: survey.title,
+      description: survey.description,
+      user_id: user.id,
+      link: process.env.URL_MAIL
+    };
+
+
+    const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+
+    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+      where: [{user_id:user.id}, {value: null}],
+      relations: ["user", "survey"]
+    })
+
+    if (surveyUserAlreadyExists) {
+      await SendMailService.execute(email, survey.title, variables, npsPath)
+      return response.json(surveyUserAlreadyExists)
     }
 
     const surveyUser = surveysUsersRepository.create({
-      user_id: userAlreadyExists.id,
+      user_id: user.id,
       survey_id,
-    })
-    await surveysUsersRepository.save(surveyUser)
+    });
+    await surveysUsersRepository.save(surveyUser);
 
-    await SendMailService.execute(email,survey.title, survey.description)
 
-    return response.json(surveyUser)
+
+
+    await SendMailService.execute(email, survey.title, variables, npsPath);
+
+    return response.json(surveyUser);
   }
-
 }
 
-export default SendMailController
+export default SendMailController;
--- src/services/SendMailService.ts
+++ src/services/SendMailService.ts
@@ -1,4 +1,6 @@
 import nodemailer, { Transporter } from "nodemailer";
+import handlebars from "handlebars";
+import fs from "fs";
 
 class SendMailService {
   private client: Transporter;
@@ -18,16 +20,22 @@ class SendMailService {
     });
   }
 
-  async execute(to:string, subject: string, body: string) {
+  async execute(to: string, subject: string, variables: object, path: string) {
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+    const mailTemplateParse = handlebars.compile(templateFileContent);
+
+    const html = mailTemplateParse(variables);
+
     const message = await this.client.sendMail({
       to,
       subject,
-      html: body,
-      from: "NPS <noreplay@nps.com.br>"
-    })
+      html,
+      from: "NPS <noreplay@nps.com.br>",
+    });
 
-    console.log('Message sent: %s', message.messageId);
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+    console.log("Message sent: %s", message.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(message));
   }
 }
 
