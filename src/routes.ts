10788973c8e3bf28a0525bde640f9d041b26665f
Ricardo Gorki <ricardo.gorki@gmail.com>
2021-02-27 22:49:08 -0300

    finished nlw

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,16 @@ import { Router } from 'express'
 import UserController from './controllers/UserController'
 import SurveysController from './controllers/SurveysController'
 import SendMailController from './controllers/SendMailController'
+import AnswerController from './controllers/AnswerController'
+import NpsController from './controllers/NpsController'
 
 const routes = Router()
 
 const userController = new UserController()
 const surveysController = new SurveysController()
 const sendMailController = new SendMailController()
+const answerController = new AnswerController()
+const npsController = new NpsController()
 
 routes.post('/users', userController.create)
 
@@ -18,5 +22,11 @@ routes.get('/surveys', surveysController.show)
 
 
 routes.post('/sendMail', sendMailController.execute)
+
+routes.get('/answers/:value', answerController.execute)
+
+routes.get('/nps/:survey_id', npsController.execute)
+
+
 
 export default routes;
